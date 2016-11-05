aa5fc4f26db1f2a3b8c21393aaaac7d5aec1d9a4
Ludwig Possie <ludwig@possiefamily.com>
2016-11-05 00:47:47 -0600

    Initial Commit

---
+++ PlexToVera.py
@@ -0,0 +1,129 @@
+import urllib.request
+import urllib.parse
+import urllib.error
+import xml.etree.ElementTree as etree
+import time
+import configparser
+import logging
+import json
+
+#Load configuration file
+config = configparser.ConfigParser()
+config.sections()
+config.read('./PlexToVera.conf')
+
+# Set up logging
+LogLevel = int(config['GENERAL']['LogLevel'])
+logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p', filemode='w', filename='./PlexToVera.log', level=LogLevel)
+
+# General Variables
+DelayTime = int(config['GENERAL']['DelayTime'])
+LightStatus = "ON"
+
+# Plex Variables            
+PlexServerIP = config['PLEX']['PlexServerIP']
+PlexServerPort = config['PLEX']['PlexServerPort']
+PlexServerToken = config['PLEX']['PlexServerToken']
+PlexServerURL = 'http://' + PlexServerIP + ':' + PlexServerPort + '/status/sessions?X-Plex-Token=' + PlexServerToken
+PlexClient = config['PLEX']['PlexClient']
+
+#Vera Variables
+VeraServerIP = config['VERA']['VeraServerIP']
+VeraServerPort = config['VERA']['VeraServerPort']
+VeraMovieLightingSceneID = config['VERA']['PlexMovieOnSceneID']
+VeraBrightLightingSceneID = config['VERA']['PlexMovieOffSceneID']
+VeraPausedLightingSceneID = config['VERA']['PlexMoviePauseSceneID']
+VeraDayNightPlugidID = config['VERA']['DayNightPluginID']
+
+#Set URL requests
+DayNightStatusURL = 'http://' + VeraServerIP + ':' + VeraServerPort + '/data_request?id=status&output_format=json&DeviceNum=' + VeraDayNightPlugidID
+TurnOnMovieLightingModeURL = 'http://' + VeraServerIP + ':' + VeraServerPort + '/data_request?id=lu_action&output_format=json&serviceId=urn:micasaverde-com:serviceId:HomeAutomationGateway1&action=RunScene&SceneNum=' + VeraMovieLightingSceneID
+TurnOnBrightLightingModeURL = 'http://' + VeraServerIP + ':' + VeraServerPort + '/data_request?id=lu_action&output_format=json&serviceId=urn:micasaverde-com:serviceId:HomeAutomationGateway1&action=RunScene&SceneNum=' + VeraBrightLightingSceneID
+TurnOnMidLightingModeURL = 'http://' + VeraServerIP + ':' + VeraServerPort + '/data_request?id=lu_action&output_format=json&serviceId=urn:micasaverde-com:serviceId:HomeAutomationGateway1&action=RunScene&SceneNum=' + VeraPausedLightingSceneID
+
+#General Vera request function
+def veraRequest(URL):
+    response = urllib.request.urlopen(URL)
+    content = response.read()
+    data = json.loads(content.decode("utf8"))
+
+    return data
+
+#Request day or night status function
+def veraDayNight(URL):
+
+    DayNightDevice = 'Device_Num_' + VeraDayNightPlugidID
+    data = veraRequest(URL)
+    dayNightStatus = (data[DayNightDevice]['states'][0]['value'])
+
+    return dayNightStatus
+
+#Run scene execution on Vera function
+def veraScene(URL):
+    data = veraRequest(URL)
+    VeraStatus = (data['u:RunSceneResponse']['OK'])
+
+    if VeraStatus == "OK":
+        logging.info('Command successfully sent')
+    else:
+             logging.info('Command Failed: Try %s of 5 : %s', x+1, RestURL)
+             time.sleep(1)
+
+#---------------------------
+# Open the Status_sessions.xml from the Plex Server
+# Read the xml file looking for our desired Plex Client
+# Print Status
+
+logging.info('============================================')
+logging.info('--------------------------------------------')
+logging.info('Starting PlexToVera Script')
+logging.info('--------------------------------------------')
+logging.info('Plex Server : %s', PlexServerURL)
+logging.info('Plex Client : %s', PlexClient)
+logging.info('VERA Server : http://%s:%s', VeraServerIP, VeraServerPort)
+logging.info('--------------------------------------------')
+
+print('--------------------------------------------')
+print('Starting PlexToVera Script')
+print('No further output here. See logfile for more details.')
+print('Press Ctrl-C to Stop Program')
+print('--------------------------------------------')
+
+#Main
+while True:
+     logging.debug('-----------------------------------------')
+     try:
+         u = urllib.request.urlopen(PlexServerURL)
+         tree = etree.parse(u)
+         root = tree.getroot()
+         try:
+             value = root.find(".//*[@machineIdentifier='" + PlexClient + "']")
+             MovieStatus = value.attrib["state"]
+         except AttributeError:
+             MovieStatus = "stopped"
+         logging.debug('Movie is %s', MovieStatus)
+
+         #Determine if night, if not it will skip light controls
+         if veraDayNight(DayNightStatusURL) == '0':
+             if MovieStatus == "playing":
+                 if (LightStatus == "ON") or (LightStatus =="DIM"):
+                     logging.info('Turning LIGHTS DOWN')
+                     veraScene(TurnOnMovieLightingModeURL)
+                     LightStatus = "OFF"
+             elif MovieStatus == "paused":
+                 if LightStatus == "OFF":
+                     logging.info('Turning LIGHTS DIM')
+                     veraScene(TurnOnMidLightingModeURL)
+                     LightStatus = "DIM"
+             else:
+                 if (LightStatus == "OFF") or (LightStatus =="DIM"):
+                     logging.info('Turning Lights UP')
+                     veraScene(TurnOnBrightLightingModeURL)
+                     LightStatus = "ON"
+
+         logging.debug('Waiting for next polling interval.')
+         time.sleep(DelayTime)
+     except urllib.error.URLError as e:
+             print(e.reason)
+             print ('Plex Server Not Responding... waiting 1 minute.')
+             time.sleep(60)
---
+++ README.md
@@ -0,0 +1,26 @@
+PlexToVera
+
+First of all, I just want to give credit where credit is due.  Thanks to bsmith993 original PlexToInstean design, I was able to redesign his original app so it would work the Vera.  I am by no means a developer by any stretch of the imagination, so if anyone find a better alternative or code corrections, I'm always open to suggestions.  
+
+Small, standalone Python script that monitors a Plex Server to determine if a specific Plex Client is playing a movie, and will turn Insteon lights up or down based on playback status.
+
+GOAL
+
+When you start watching a movie, dim your Zwave lights to a movie scene you have created. When you pause or stop the movie, return the movie lighting to a normal lighting mode.
+
+REQUIREMENTS
+
+- Plex Media Server
+- Plex Client
+- Vera contolled lighting, with a movie mode scene,  a NON movie mode scene and a paused movie scene. Movie mode intended to be off, non movie mode intended to be bright and paused mode intended to be dimmed.
+
+INSTALLATION
+
+- You must have Python installed. (Google it if you need help) This is written for Python 3.4 
+- There is no installation for the script itself. Simply place the files in the following folder /opt/PlexToVera and then schedule a job using cron (or however else you'd like to trigger it). 
+- Make a copy of the PlexToVera.conf.default file and rename it PlexToVera.conf and modify the file to your variables and fire up the script. 
+- The script will continuously run. It is low memory and CPU usage so I just let it be. -The script will fail right now if the Plex Server is stopped. So I have to restart it sometimes.
+
+USAGE
+
+Here's the plan... read the xml file from the Plex Server to see if a user defined Plex Home Theater Client is playing a movie. If it is and it's night time, set the lights to movie mode. When it pauses it dims the lights or if it stops, bring em up.
